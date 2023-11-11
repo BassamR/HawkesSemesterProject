9e2f68629c53c1f0c6faee9743742bb1e42de365
Bassam El Rawas <bassamr99@gmail.com>
2023-11-11 21:39:10 +0100

    Improve plotting

--- plot.py
+++ plot.py
@@ -6,29 +6,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Plot counting process
-def plot_counting_process(P) -> None:
+def plot_counting_process(P, axe) -> None:
     """
     P: arrival times
+    axe: Axe for plotting
     """
     def counting_process(arrival_times):
         return [i + 1 for i in range(len(arrival_times))]
 
-    plt.figure()
-    plt.step(x=P, y=counting_process(P), where='post', label='N(t)')
-    plt.grid()
-    plt.xlabel('Time')
-    plt.ylabel('Count')
-    plt.scatter(P, [0] * len(P), marker='x', color='red', label='Arrival times')
-    plt.legend(loc='upper left')
+    axe.step(x=P, y=counting_process(P), where='post', label='N(t)')
+    axe.grid()
+    axe.set_xlabel('Time')
+    axe.set_ylabel('Count')
+    axe.scatter(P, [0] * len(P), marker='x', color='red', label='Arrival times')
+    axe.legend(loc='upper left')
 
     return
 
 # Plot intensity function
-def plot_intensity(P, T, mu, h) -> None:
+def plot_intensity(P, T, mu, h, axe) -> None:
     """
     P: array of arrivals
     T: arrivals contained in the interval [0, T]
     h: excitation function
+    axe: Axe for plotting
     """
     # Function to calculate the sum of f(t - ti)
     def sum_excitations(t, t_values, f):
@@ -37,16 +38,26 @@ def plot_intensity(P, T, mu, h) -> None:
     time_vector = np.linspace(0, T, 500)
     intensity = mu + np.array([sum_excitations(t, P, h) for t in time_vector])
 
-    plt.figure()
-    plt.plot(time_vector, intensity, label='$\lambda^*(t)$')
-    plt.grid()
-    plt.xlabel('Time')
-    plt.ylabel('Intensity')
-    plt.scatter(P, [0] * len(P), marker='x', color='red', label='Arrival times')
-    plt.legend()
+    axe.plot(time_vector, intensity, label='$\lambda^*(t)$', linewidth=0.8)
+    axe.grid()
+    axe.set_xlabel('Time')
+    axe.set_ylabel('Intensity')
+    axe.scatter(P, [0] * len(P), marker='x', color='red', label='Arrival times')
+    axe.legend()
 
     return
 
-# TODO: Plot histogram of arrivals
-def plot_histogram(P) -> None:
+# Plot histogram of arrivals
+def plot_histogram(P, T, axe) -> None:
+    """
+    :param P: Array of arrival times.
+    :param T: Arrival times are contained in [0,T].
+    :param axe: Axe for plotting.
+    """
+
+    axe.hist(P, bins=T//5, edgecolor='black')
+    axe.set_xlabel('Arrival Times')
+    axe.set_ylabel('Frequency')
+    #axe.set_title('Histogram of Arrival Times')
+
     return
